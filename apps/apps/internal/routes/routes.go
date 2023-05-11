03740b46837a87905fa0d52fbe978c1032ecf85c
wt3022 <>
2023-05-12 02:46:55 +0900

    [add] 初期環境構築

--- apps/apps/internal/routes/routes.go
+++
@@ -1,9 +0,0 @@
-package routes
-
-import (
-	"github.com/labstack/echo/v4"
-	"gorm.io/gorm"
-)
-
-func InitRoutes(e *echo.Echo, db *gorm.DB) {
-}
--- apps/apps/scripts/ent_c.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-go run -mod=mod entgo.io/ent/cmd/ent new $1
