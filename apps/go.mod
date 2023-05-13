3e76d8ebdd24b0b48b4f0189879ec273b9288157
wt3022 <>
2023-05-13 17:22:23 +0000

    [update] go-slq-mysqlの追加

--- apps/go.mod
+++ apps/go.mod
@@ -24,13 +24,14 @@ require (
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -41,6 +42,8 @@ require (
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/swaggo/echo-swagger v1.4.0 // indirect
+	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/zclconf/go-cty v1.8.0 // indirect
--- apps/internal/db/db.go
+++ apps/internal/db/db.go
@@ -3,12 +3,14 @@ package db
 import (
 	"book_manage_app/ent"
 	"book_manage_app/internal/config"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"os"
 	"time"
 
-	"entgo.io/ent/dialect/sql"
+	entsql "entgo.io/ent/dialect/sql"
 )
 
 type DatabaseConfig struct {
@@ -28,30 +30,31 @@ func NewDB() (*ent.Client, error) {
 	}
 
 	dbConfig := DatabaseConfig{
-		Username: os.Getenv("DB_USERNAME"),
-		Password: os.Getenv("DB_PASSWORD"),
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		DBName:   os.Getenv("DB_NAME"),
+		Username:     os.Getenv("DB_USERNAME"),
+		Password:     os.Getenv("DB_PASSWORD"),
+		Host:         os.Getenv("DB_HOST"),
+		Port:         os.Getenv("DB_PORT"),
+		DBName:       os.Getenv("DB_NAME"),
 		MaxIdleConns: serverConfig.DataBase.MaxIdleConns,
 		MaxOpenConns: serverConfig.DataBase.MaxOpenConns,
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		dbConfig.Username,
 		dbConfig.Password,
 		dbConfig.Host,
 		dbConfig.Port,
-		dbConfig.DBName)
+		dbConfig.DBName,
+	)
 
-	drv, err := sql.Open("mysql", dsn)
-    if err != nil {
-        log.Fatalf("データベースへの接続に失敗しました: %v\n", err)
-    }
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("データベースへの接続に失敗しました: %v\n", err)
+	}
+	db.SetMaxIdleConns(dbConfig.MaxIdleConns)
+	db.SetMaxOpenConns(dbConfig.MaxOpenConns)
+	db.SetConnMaxLifetime(time.Hour)
 
-    db := drv.DB()
-    db.SetMaxIdleConns(dbConfig.MaxIdleConns)
-    db.SetMaxOpenConns(dbConfig.MaxOpenConns)
-    db.SetConnMaxLifetime(time.Hour)
-    return ent.NewClient(ent.Driver(drv)), nil
+	drv := entsql.OpenDB("mysql", db)
+	return ent.NewClient(ent.Driver(drv)), nil
 }
