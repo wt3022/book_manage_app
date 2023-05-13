6f6166befe6316250d7a25fe346e7853a3e18623
wt3022 <>
2023-05-13 17:26:09 +0000

    [update] お試しコードの削除とechoサーバーの起動処理追加

--- apps/cmd/book_manage_app/main.go
+++ apps/cmd/book_manage_app/main.go
@@ -1,41 +1,9 @@
 package main
 
 import (
-    "context"
-    "log"
-    "book_manage_app/ent"
-
-    "entgo.io/ent/dialect"
-    _ "github.com/mattn/go-sqlite3"
-)
-
-func main() {
-    // インメモリーのSQLiteデータベースを持つent.Clientを作成します。
-    client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
-    if err != nil {
-        log.Fatalf("failed opening connection to sqlite: %v", err)
-    }
-    defer client.Close()
-    ctx := context.Background()
-    // 自動マイグレーションツールを実行して、すべてのスキーマリソースを作成します。
-    if err := client.Schema.Create(ctx); err != nil {
-        log.Fatalf("failed creating schema resources: %v", err)
-    }
-    // 出力します。
-	task1, err := client.User.Create().Save(ctx)
-    if err != nil {
-        log.Fatalf("failed creating a todo: %v", err)
-    }
-    log.Println(task1)
-}
-
-
-
-/*
-import (
 	_ "book_manage_app/docs"
-	"book_manage_app/internal/api/routes"
 	"book_manage_app/internal/db"
+	"book_manage_app/internal/routes"
 	"fmt"
 	"log"
 	"os"
@@ -50,7 +18,6 @@ import (
 // @BasePath	/
 // @in			header
 // @name		hoge
-
 func main() {
 	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("サーバー起動時に.envファイルが読み込めませんでした。")
@@ -67,4 +34,3 @@ func main() {
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))))
 }
-*/
